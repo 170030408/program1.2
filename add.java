a3b4fc8c264790e317e6e9ec50d2d5cd3f8e57fa
170030408 <59306493+170030408@users.noreply.github.com>
2019-12-28 15:13:04 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class add {
+
+   public static void main(String[] args) {
+        int a = 25;
+int b = 14;
+int c = a + b;
+      System.out.println("Sum of these numbers: "+c);
+   }
+}
